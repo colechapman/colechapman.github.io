f3050d2e08da39567ff05f557a3d0ee8b9f38bfd
cole chapman <cole.chapman15@gmail.com>
2024-03-05 19:29:09 -0700

    Update update_forecast.rb

--- update_forecast.rb
+++ update_forecast.rb
@@ -1,15 +1,14 @@
 require 'shipping_forecast'
-require 'fileutils'
 
 # Fetch the forecast for all locations
 all_forecasts = ShippingForecast.all
 
-# Prepare the HTML content
-html_content = "<h1>Latest Shipping Forecast</h1>\n<ul>"
+# Start building the HTML content for the Shipping Forecast section
+html_content = "<div id='shipping-forecast'>\n<h2>Latest Shipping Forecast</h2>\n<ul>\n"
 all_forecasts.each do |location, forecast|
-  html_content += "<li><strong>#{location}</strong>: #{forecast[:wind]}, #{forecast[:seas]}, #{forecast[:weather]}, #{forecast[:visibility]}</li>"
+  html_content += "<li><strong>#{location}</strong>: #{forecast[:wind]}, #{forecast[:seas]}, #{forecast[:weather]}, #{forecast[:visibility]}</li>\n"
 end
-html_content += "</ul>"
+html_content += "</ul>\n</div>"
 
-# Write to an HTML file
-File.write('forecast.html', html_content)
+# Write to an HTML file that will be included in the index.html
+File.write('forecast_fragment.html', html_content)
